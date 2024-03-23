eb5d9eb5430202a4a923b7bfe9180c3f1454fd22
frezdev <carlos.andres.ff2912@gmail.com>
2024-03-23 11:20:28 -0500

    Update header component - Add styles - Add link for navigate to home "/"

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link"
+
 export const Header = () => {
   return (
-    <h1>Header</h1>
+    <header className="mb-2 p-5 shadow-sm">
+      <h1 className="font-bold">
+        <Link href="/">Frontend Test</Link>
+      </h1>
+    </header>
   )
 }
