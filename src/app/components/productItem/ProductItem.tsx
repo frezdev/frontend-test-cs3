c7854e4c101cff67c9afcc1e0e03ae9397b2d68d
frezdev <carlos.andres.ff2912@gmail.com>
2024-03-23 11:23:56 -0500

    Update ProductTable & ProductItem component

--- src/app/components/productItem/ProductItem.tsx
+++ src/app/components/productItem/ProductItem.tsx
@@ -1,16 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 import { type Product } from '@/app/types';
 
-export const ProductItem = ({ product } : {product: Product}) => {
-  const {id, title, price, permalink, currency_id, thumbnail} = product;
+export const ProductItem = ({ product }: { product: Product }) => {
+  const { id, title, price, permalink, currency_id, thumbnail } = product;
+
   return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{price.toLocaleString('es-AR', {currency: currency_id, style: 'currency'})}</td>
-      <td>{permalink}</td>
-      <td>
-        <img src={thumbnail} alt={`Imagen de ${title}`} loading='lazy' />
+    <tr className="bg-white border-b hover:bg-gray-50">
+      <td className="px-4 py-2 font-semibold text-gray-900">
+        {id}
+      </td>
+      <td className="px-4 py-2 font-semibold text-gray-900 min-w-56">
+        {title}
+      </td>
+      <td className="px-4 py-2 font-semibold text-gray-900">
+        {price.toLocaleString('es-AR', { currency: currency_id, style: 'currency' })}
+      </td>
+      <td className="px-4 py-2">
+        <a href={permalink} target='_blank' className="font-medium text-blue-500 hover:underline">{permalink}</a>
+      </td>
+      <td className="p-4">
+        <img src={thumbnail} loading='lazy' className="w-16 md:w-32 max-w-full max-h-full rounded" alt={title} />
       </td>
     </tr>
   )
--- src/app/components/productsTable/ProductsTable.tsx
+++ src/app/components/productsTable/ProductsTable.tsx
@@ -1,24 +1,74 @@
-import { Product } from '@/app/types'
+'use client'
+import { useRef, useState } from 'react';
+import { Paging, Product } from '@/app/types'
 import { ProductItem } from '@/app/components/productItem';
+import { PaginationBar } from '@/app/components/paginationBar';
+import { TableSkeleton } from '@/app/components/tableSkeleton';
+import { getProducts } from '@/app/services';
 
-export const ProductsTable = async ({ products }: { products: Product[] }) => {
+const tableHeadTitles = [
+  'Id',
+  'Nombre',
+  'Precio',
+  'MercadoEnlace',
+  'Imagen'
+]
+
+interface Props {
+  products: Product[],
+  paging: Paging,
+  category: string | null
+}
+export const ProductsTable = ({ products, paging, category }: Props) => {
+  const [curretData, setCurretData] = useState({ products, paging });
+  const [pageNumber, setPageNumber] = useState(curretData.paging.offset);
+  const [loading, setLoading] = useState(false);
+  const tableContainer = useRef<HTMLDivElement>(null);
+
+  const handleClickNext = async () => {
+    setLoading(true);
+    const data = await getProducts({ offset: pageNumber + 1, category });
+    if (!data) return;
+    setCurretData(data)
+    setPageNumber(prev => prev + 1)
+    tableContainer.current?.scroll(0, 0);
+    setLoading(false);
+  }
+
+  const handleClickPrev = async () => {
+    setLoading(true);
+    const data = await getProducts({ offset: pageNumber - 1, category });
+    if (!data) return;
+    setCurretData(data)
+    setPageNumber(prev => prev - 1)
+    tableContainer.current?.scroll(0, 0);
+    setLoading(false);
+  }
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Nombre</th>
-          <th>Precio</th>
-          <th>MercadoEnlace</th>
-          <th>Imagen</th>
-        </tr>
-      </thead>
-      <tbody>
-        {products?.map(product => (
-          <ProductItem product={product} key={product.id} />
-        ))}
-      </tbody>
-    </table>
+    <div className='px-3'>
+      <div ref={tableContainer} className="relative overflow-x-auto max-h-[calc(100dvh-156px)] shadow-md sm:rounded-sm">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-400">
+            <tr>
+              {tableHeadTitles.map(title => (
+                <th scope="col" className='px-10 py-3' key={title}>{title}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+
+            {
+              loading
+                ? <TableSkeleton />
+                : (curretData.products?.map((product) => (
+                  <ProductItem product={product} key={product.id} />
+                )))
+            }
+          </tbody>
+        </table>
+      </div>
+      <PaginationBar paging={curretData.paging} clickNext={handleClickNext} clickPrev={handleClickPrev} />
+    </div>
   )
 }
