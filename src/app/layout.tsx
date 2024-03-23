c2b8a3c64eb7f4e132ac402051444c45a55c4a39
frezdev <carlos.andres.ff2912@gmail.com>
2024-03-23 11:15:16 -0500

    Add fetch data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Header } from "@/app/components/header";
+import { ListOfCategories } from "@/app/components/listOfCategories";
 import "./globals.css";
+import { getProducts, getCategories } from "./services";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,15 +12,26 @@ export const metadata: Metadata = {
   description: "Technical test for Frontend Developer",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const data = await getProducts();
+
+  if (!data) return;
+  const allCategories = await getCategories(data.products);
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <Header />
+        <div className='grid grid-cols-[320px_1fr]'>
+          <aside className=''>
+            <ListOfCategories categories={allCategories} />
+          </aside>
+          <main>{children}</main>
+        </div>
       </body>
     </html>
   );
